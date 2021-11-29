f6001eae973d724638c08b38efd794f9b102e65c
Jeff Terry <terryj@iit.edu>
2021-11-28 22:49:43 -0600

    Initial Commit

---
+++ Shared/Atomic_Structure_Calculations__Non_Relativistic_App.swift
@@ -0,0 +1,17 @@
+//
+//  Atomic_Structure_Calculations__Non_Relativistic_App.swift
+//  Shared
+//
+//  Created by Jeff Terry on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Atomic_Structure_Calculations__Non_Relativistic_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
